9b549a3a8238d353810df93ccc1e674af2bdf361
miriam <m.moreno.clavijo@gmail.com>
2022-02-25 19:33:02 +0100

    creando un modulo personalizado para separar los imports de primeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import {ButtonModule} from 'primeng/button';
-import {CardModule} from 'primeng/card';
+import { PrimeNGModule } from './prime-ng/prime-ng.module';
+
 
 
 @NgModule({
@@ -11,8 +11,7 @@ import {CardModule} from 'primeng/card';
   ],
   imports: [
     BrowserModule,
-    ButtonModule,
-    CardModule
+    PrimeNGModule
 
   ],
   providers: [],
