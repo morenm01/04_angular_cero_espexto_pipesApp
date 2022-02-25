4e5cc61381e64a646a9a6034d6c925f1fe7b3afd
miriam <m.moreno.clavijo@gmail.com>
2022-02-25 19:21:03 +0100

    importando primeNG, y creando los estilos y el card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+  nombre = 'miriam moreno';
+  numero = 1000;
+  obj = {
+    objeto: 'Mi objeto',
+    valor: 'el mundo'
+  }
+
+  cambiarNombre(): void{
+    console.log(this.nombre);
+    console.log(this.numero);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule,
+    CardModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
