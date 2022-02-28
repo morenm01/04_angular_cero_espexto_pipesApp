699ec0282371a6818715378f7f1247fd697e0e57
miriam <m.moreno.clavijo@gmail.com>
2022-02-28 12:10:47 +0100

    creando la demo de menu de primeNG y empezando el víedeo 139 MenuBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PrimeNGModule } from './prime-ng/prime-ng.module';
+import { MenuDemoComponent } from './menu-demo/menu-demo.component';
+import { SharedModule } from './shared/shared.module';
+
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuDemoComponent
   ],
   imports: [
     BrowserModule,
-    PrimeNGModule
+    PrimeNGModule,
+    SharedModule
 
   ],
   providers: [],
